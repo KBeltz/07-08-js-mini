d2d98cf98ef3a90543274b468a4893880e3ab9dd
Kristin Beltz <beltz08@me.com>
2015-07-08 17:19:36 -0500

    Used online tutorial. Currently functional. May require refactoring.

--- sticky-shrinking-header/global.js
+++ sticky-shrinking-header/global.js
@@ -1,103 +1,16 @@
-function init() {
-    window.addEventListener('scroll', function(e) {
-        var distanceY = window.pageYOffset || document.documentElement.scrollTop;
-            var shrinkOn = 300;
-            //Returns the first element within the document (using depth-first pre-order traversal of the document's nodes) that matches the specified group of selectors.
-            topBar = document.querySelector("top_bar");
-        if (distanceY > shrinkOn) {
-            classie.add(top_bar,"smaller");
-        } else {
-            if (classie.has(top_bar,"smaller")) {
-                classie.remove(top_bar,"smaller");
-            }
-        }
-    });
+var pagetop, menu, yPos;
+function yScroll(){
+	pagetop = document.getElementById('pagetop');
+	menu = document.getElementById('menu');
+	yPos = window.pageYOffset;
+	if(yPos > 150){
+		pagetop.style.height = "36px";
+		pagetop.style.paddingTop = "8px";
+		menu.style.height = "0px";
+	} else {
+		pagetop.style.height = "120px";
+		pagetop.style.paddingTop = "50px";
+		menu.style.height = "50px";
+	}
 }
-
-/*!
- * classie v1.0.1
- * class helper functions
- * from bonzo https://github.com/ded/bonzo
- * MIT license
- *
- * classie.has( elem, 'my-class' ) -> true/false
- * classie.add( elem, 'my-new-class' )
- * classie.remove( elem, 'my-unwanted-class' )
- * classie.toggle( elem, 'my-class' )
- */
-
-/*jshint browser: true, strict: true, undef: true, unused: true */
-/*global define: false, module: false */
-
-( function( window ) {
-
-'use strict';
-
-// class helper functions from bonzo https://github.com/ded/bonzo
-
-function classReg( className ) {
-  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
-}
-
-// classList support for class management
-// altho to be fair, the api sucks because it won't accept multiple classes at once
-var hasClass, addClass, removeClass;
-
-if ( 'classList' in document.documentElement ) {
-  hasClass = function( elem, c ) {
-    return elem.classList.contains( c );
-  };
-  addClass = function( elem, c ) {
-    elem.classList.add( c );
-  };
-  removeClass = function( elem, c ) {
-    elem.classList.remove( c );
-  };
-}
-else {
-  hasClass = function( elem, c ) {
-    return classReg( c ).test( elem.className );
-  };
-  addClass = function( elem, c ) {
-    if ( !hasClass( elem, c ) ) {
-      elem.className = elem.className + ' ' + c;
-    }
-  };
-  removeClass = function( elem, c ) {
-    elem.className = elem.className.replace( classReg( c ), ' ' );
-  };
-}
-
-function toggleClass( elem, c ) {
-  var fn = hasClass( elem, c ) ? removeClass : addClass;
-  fn( elem, c );
-}
-
-var classie = {
-  // full names
-  hasClass: hasClass,
-  addClass: addClass,
-  removeClass: removeClass,
-  toggleClass: toggleClass,
-  // short names
-  has: hasClass,
-  add: addClass,
-  remove: removeClass,
-  toggle: toggleClass
-};
-
-// transport
-if ( typeof define === 'function' && define.amd ) {
-  // AMD
-  define( classie );
-} else if ( typeof exports === 'object' ) {
-  // CommonJS
-  module.exports = classie;
-} else {
-  // browser global
-  window.classie = classie;
-}
-
-})( window );
-
-window.onload = init();
+window.addEventListener("scroll", yScroll);
