2b5849e73b26bf2f50f7f48a0dff58e03e50a01b
Kristin Beltz <beltz08@me.com>
2015-07-08 12:24:34 -0500

    Add some styling to the modal window and links.

--- modal-window/global.js
+++ modal-window/global.js
@@ -1,5 +1,16 @@
 
 function overlay() {
-	el = document.getElementById("overlay");
-	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+	cb = document.getElementById("overlay");
+	cb.style.visibility = (cb.style.visibility == "visible") ? "hidden" : "visible";
+
+	main = document.getElementById("main");
+	main.style.visibility = (main.style.visibility = "hidden");
+}
+
+function close() {
+	main = document.getElementById("main");
+	main.style.visibility = (main.style.visibility == "visible") ? "hidden" : "visible";
+
+	cb = document.getElementById("overlay");
+	cb.style.visibility = (cb.style.visibility = "hidden");
 }
