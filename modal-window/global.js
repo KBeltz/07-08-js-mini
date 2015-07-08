36b742a64fc0bdf5f0fde85fa0933a1e759a7193
Kristin Beltz <beltz08@me.com>
2015-07-08 12:31:03 -0500

    Main div now visible when closing modal window.

--- modal-window/global.js
+++ modal-window/global.js
@@ -6,11 +6,3 @@ function overlay() {
 	main = document.getElementById("main");
 	main.style.visibility = (main.style.visibility = "hidden");
 }
-
-function close() {
-	main = document.getElementById("main");
-	main.style.visibility = (main.style.visibility == "visible") ? "hidden" : "visible";
-
-	cb = document.getElementById("overlay");
-	cb.style.visibility = (cb.style.visibility = "hidden");
-}
