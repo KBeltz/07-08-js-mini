5acdbc04158cca5cb34a1714cf3c2c771cca60d5
Kristin Beltz <beltz08@me.com>
2015-07-08 22:58:29 -0500

    Menu is blank, but the transition is there.

---
+++ barcamp-mobile-nav/global.js
@@ -0,0 +1,14 @@
+// Get the DOM reference
+var dropMenu = document.getElementById("drop_menu");
+var navMenu = document.getElementById("nav_menu");
+
+// Toggle
+function toggleContent() {
+  navMenu.addEventListener("animationstart", 'click', false);
+  navMenu.addEventListener("animationend", 'click', false);
+  navMenu.style.display == "block" ? navMenu.style.display = "none" : navMenu.style.display = "block";
+}
+
+drop_menu.onclick = toggleContent;
+
+navMenu.className = "slidein";
